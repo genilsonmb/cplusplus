faaa818cc581ca6486055e3d810dd13a90ff516e
genilsonmb <genilson1986@gmail.com>
2018-05-03 15:50:06 -0300

    Create 03_maio18_herancamultipla.cpp

---
+++ 03_maio18_herancamultipla.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class Pai{
+	public:
+	//tentar com "virtual void m1()" para verificar diferença
+		virtual void m1(){
+			cout << "PaiM1\n";
+		}
+	//testar com virtual e sem aqui tambem
+		void m2() {
+			m1();
+			cout << "PaiM2\n";
+			
+		}
+	
+};
+
+class Filho:public Pai{
+public:
+	//testar com virtual e sem aqui tambem
+	void m1(){
+		cout << "FilhoM1\n";
+	}
+	
+	
+};
+
+
+
+int main()
+{
+	Pai p;
+	p.m2();
+	Filho f;
+	f.m2();
+	Filho f2;
+	
+	Pai *p1 = &f2;
+	p1->m2();
+	 
+	Pai &p2 = f2;
+	p2.m2();
+	
+	Pai p3 = f2;
+	p3.m2();
+	
+	
+	return 0;
+}
